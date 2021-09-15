08fc1592864404709bf937d09d3f4881fb76d310
karan2021 <justkarnaa@gmail.com>
2021-09-15 15:24:01 +0530

    ..

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class filexercise2
+{
+    public static void Main()
+    {
+        string fileName = @"C:\Training\CSharp\ConsoleApp4\ConsoleApp4\mytest.txt";
+        Console.Write("Remove a file from the disk (at first create the file)\n");
+
+        if (File.Exists(fileName))
+        {
+            File.Delete(fileName);
+            Console.WriteLine(" The file {0} deleted successfully", fileName);
+        }
+        else
+        {
+            Console.WriteLine(" File does not exist");
+            Console.ReadKey();
+        }
+    }
+}
